d4fecfbc7e5abcdc98041c960895c4c323b06301
Xavier Freire <40183534+xavfo@users.noreply.github.com>
2026-06-19 11:32:13 -0500

    corrección compatibilidad

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -10,7 +10,7 @@ android {
 
     defaultConfig {
         applicationId = "com.qsoftware.qcrm"
-        minSdk = 24
+        minSdk = 21
         targetSdk = 36
         versionCode = 1
         versionName = "1.0"
